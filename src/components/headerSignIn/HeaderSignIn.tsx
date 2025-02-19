56be67037b180fa71516375ccea38a8b40ae25a6
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-02-19 16:12:23 +0200

    sign in header and form components

---
+++ src/components/headerSignIn/HeaderSignIn.tsx
@@ -0,0 +1,11 @@
+import './headerSignIn.css'
+
+export default function HeaderSignIn() {
+    return (
+        <div className="headerSignIn">
+            <h1>Welcome Back!</h1>
+            <p>To keep connected with us please Sign in with your credentials</p>
+        </div>
+    )
+
+}
---
+++ src/components/signInForm/SignInForm.tsx
@@ -0,0 +1,12 @@
+import './signInForm.css'
+
+export default function SignInForm() {
+    return (
+        <div className="signInForm">
+            <h2>Project Management App</h2>
+            <form className="signInForm">
+
+            </form>
+        </div>
+    )
+}
