1e7c4cf3a03cc0dcd6ef863bca85b18e6236f6fa
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-02-21 16:41:07 +0200

    handle signin form

--- src/components/signInForm/SignInForm.tsx
+++ src/components/signInForm/SignInForm.tsx
@@ -1,14 +1,45 @@
 import './signInForm.css'
 import picProfile from '../../assets/picProfile.png'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useForm } from "react-hook-form";
+import { auth } from "../../../firebaseconfig.ts";
+import axios from "axios";
 
 export default function SignInForm() {
+interface SignInFormData {
+    username: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+    administrator: boolean,
+    email: string,
+}
+        const { register, handleSubmit, formState: { errors } } = useForm<SignInForm>();
+        const onSubmit = async (data: SignInFormData) => {
+            try {
+                const userCredential = await signInWithEmailAndPassword(auth, data.email, data.password);
+                const token = await userCredential.user.getIdToken();
+
+                await axios.post("/api/auth", { token });
+
+                alert("Signed in successfully!");
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        }
+
     return (
-        <div className="signInForm">
+        <div className="signInForm" onSubmit={handleSubmit(onSubmit)}>
             <h2>Project Management App</h2>
             <img className="profileImg" src={picProfile} alt="profile-img"/>
             <form className="signInForm">
-                <input className="inputName" type="text" name="username" placeholder="Username" />
-                <input className="inputPassword" type="password" name="password" placeholder="Password" />
+                <input {...register("email", { required: "Username is required" })}
+                       className="inputName" type="text" name="username" placeholder="Username" />
+                {errors.username && <p>{errors.username.message}</p>}
+
+                <input {...register("password", { required: "Password is required" })}
+                       className="inputPassword" type="password" name="password" placeholder="Password" />
+                {errors.password && <p>{errors.password.message}</p>}
                 <button className="BtnSignIn" type="submit">Sign in</button>
             </form>
         </div>
