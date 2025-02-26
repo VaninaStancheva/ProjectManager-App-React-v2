284b1502da61ee9b9341e5cd134479502c91eec3
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-02-26 14:07:01 +0200

    first navigate stepst to dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import SignInPage from "./pages/SigninPage/Signin.tsx";
+import Dashboard from "./pages/dashboard/Dashboard.tsx";
 
 function App() {
 
   return (
-    <>
-      <SignInPage />
-    </>
+        <Router>
+            <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+            <SignInPage />
+
+        </Router>
+
   )
 }
 
---
+++ src/components/nav-bar/Nav-bar.tsx
@@ -0,0 +1,14 @@
+import './nav-bar.css';
+
+export default function NavBar() {
+    return (
+        <nav className="nav-bar">
+            <h1>AppStack</h1>
+            <ul className="nav-bar-list">
+                <li className="nav-item">Projects</li>
+                <li className="nav-item">Users</li>
+                <li className="nav-item">Teams</li>
+            </ul>
+        </nav>
+    )
+}
