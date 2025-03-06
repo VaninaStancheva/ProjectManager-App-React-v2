a79ea6309291082210fe8ff0fb2e8b1719bc0fb6
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-03-06 11:44:57 +0100

    dashboard single card

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import NavBar from "../../components/nav-bar/Nav-bar.tsx";
 import {Link} from "react-router";
 import {Fab} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
+import DashboardSingleCard from "../../components/dashboard-components/dashboard-card/dash-single-card.tsx";
 
 
 export default function Dashboard() {
@@ -11,10 +12,12 @@ export default function Dashboard() {
             <Link to="/dashboard" style={{textDecoration: "none", color: "inherit"}}>
                 <NavBar/>
             </Link>
+            <DashboardSingleCard />
                 <Fab className="createBtn" variant="extended" size="medium" color="primary" aria-label="add">
                     <AddIcon/>
                     Create Project
                 </Fab>
+
         </div>
     )
 }
