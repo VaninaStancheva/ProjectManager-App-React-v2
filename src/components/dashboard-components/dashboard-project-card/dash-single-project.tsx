d276fb5fab8a9433abb412fb9430305044fe16a2
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-03-17 16:30:29 +0200

    adding new component and style

---
+++ src/components/dashboard-components/dashboard-project-card/dash-single-project.tsx
@@ -0,0 +1,12 @@
+import './dash-single-project.css'
+
+export default function DashboardProjectCard() {
+    return (
+        <div className="dashboard-projectCard-container">
+            <div className="dashboard-projectCard-content">
+                <h2 className="dashboard-projectCard-title"> Project </h2>
+                <p className = "dashboard-projectCard-description"> sdfkldfklkgfwakgawjg </p>
+            </div>
+        </div>
+    )
+}
